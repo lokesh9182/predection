618e4e82b85a8930fcf0869727dfb7d5ad85a81a
lokesh9182 <85812339+lokesh9182@users.noreply.github.com>
2022-02-22 20:08:58 +0530

    Update urls.py

--- crop_pred/urls.py
+++ crop_pred/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crop/',include('cropapp.urls')),
+    #path('crop/',include('cropapp.urls')),
+    path('',include('cropapp.urls')),
     ]
  
